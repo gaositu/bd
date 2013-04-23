a88b990feb3b7c420efd867ebf9886642e20176a
gaositu <gaositu@126.com>
2013-04-23 21:38:07 +0800

    多页面实现

--- js/zside.js
+++ js/zside.js
@@ -5,6 +5,9 @@
  *	EMAIL:	gao_st@126.com
  */
 ;( function ( window, $ ) {
+	var NO_NEED_LOAD_DATA = [ '#main' ],
+		DATA_MAPPING = [ 'v', '#css', '#css3', '#js', '#jquery', '#html5', '#php', '#sql', '#srsy', '#about' ];
+	
 	$( document ).ready( function () {
 		bindAnchorEvt();
 	});
@@ -25,6 +28,56 @@
 			});
 			
 			e.preventDefault();
+			
+			loadDataById( tmpId );
 		});
 	}
+	
+	function loadDataById ( id ) {
+		var index;
+		if ( $.inArray( id, NO_NEED_LOAD_DATA ) > -1 ) {
+			return;
+		}
+		
+		index = $.inArray( id, DATA_MAPPING );
+		if ( index > 0 ) {
+			getData( index, id );
+		}
+		
+	}
+	
+	function getData( index, id ) {
+		var ls = window.localStorage,
+			length = ls.length, 
+			i,
+			tmpKey,
+			tmpArr = [];
+		for ( i=0; i<length; i++ ) {
+			tmpKey = ls.key(i);
+			if ( tmpKey.indexOf( index ) == 0 ) {
+				tmpArr.push( tmpKey );
+			}
+		}
+		
+		renderUL( tmpArr, id );
+		
+	}
+	
+	function renderUL ( arr, id ) {
+		var str;
+		if ( arr ) {
+			str = '<li>' + arr.join('</li><li>') + '</li>';
+		}
+		
+		$( id ).find( 'ul' ).html( str );
+		// console.log( str );
+		
+		// doIScroll( id );
+	}
+	
+	/*function doIScroll( id ) {
+		document.addEventListener('DOMContentLoaded', function () { setTimeout(function () {
+			new iScroll(id, { checkDOMChanges: true });
+		}, 200); }, false);
+	}*/
 })( window, Zepto );
