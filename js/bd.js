1a596e71af1c510c5c6667a8af564850243355c5
unknown <Administrator@.(none)>
2013-05-10 09:57:52 +0800

    1

---
+++ js/bd.js
@@ -0,0 +1,199 @@
+;( function ( window, $, zside ) {
+	var myScroll, listScroll, historyScroll,
+		searchScroll,
+		appName = '面试宝典',
+		baseUrl = 'http://192.168.0.245/m/bd5/data/',
+		baseUrl = 'http://192.168.1.102/m/bd6/data/',
+		ls = window.localStorage,
+		DATA_MAPPING = [ 'v', 'css', 'css3', 'js', 'jquery', 'html5', 'php', 'sql', 'srsy', 'about' ];
+		
+	$.extend( zside, {
+		init: function () {
+			this.initScroll();
+			this.initData();
+			this.initSearch();
+		},
+		
+		initScroll: function () {
+			myScroll = new iScroll( 'wrapper', { checkDOMChanges: true } );
+			listScroll = new iScroll( 'list', { checkDOMChanges: true } );
+			historyScroll = new iScroll( 'history', { checkDOMChanges: true } );
+			searchScroll = new iScroll( 'search', { checkDOMChanges: true } );
+		},
+		
+		onHomeIconTap: function ( title, from, id, tmpHref, target ) {
+			var $masker = $( '#footer .masker'),
+				index;
+			$('#header label').text( title || appName );
+			if ( 'tabbar' != from ) {
+				$('#header #leftBtn').attr( 'href', '#home' ).attr( 'title', title ).show();
+				this.renderULById( id );
+			} else {
+				index = $(target).index() * 100;
+				$masker.css({
+					'-webkit-transition': '-webkit-transform 200ms',
+					'-webkit-transform-origin': '0% 0%', 
+					'-webkit-transform': 'translate('+index+'%, 0%) scale(1) translateZ(0px)'
+				});
+				if ( '#history' == tmpHref ) {
+					this.showHistory();
+				}
+			}
+			
+			$('#header').show();
+			listScroll.scrollTo( 0, 0, 100 );
+		},
+		
+		onLeftBtnTap: function ( title, from, id, tmpHref, target ) {
+			if ( '#list' == tmpHref ) {
+				$('#header #leftBtn').attr( 'href', '#home' ).attr( 'title', '' );
+			} else {
+				$('#header #leftBtn').hide();
+			}
+			$('#header label').text( title || appName );
+			$('#header').show();
+		},
+		
+		onRightBtnTap: function () {
+			$('#header').hide();
+		},
+		
+		initData: function () {
+			var val;
+			if ( ls ) {
+				val = ls.getItem( 'v' );
+			}
+	
+			$.get( baseUrl + 'version.json?t='+new Date().getTime(), function ( data ) {
+				if ( !val || data.v > val ) {
+					console.log( 'init data...' );
+					$.ajax({
+						url: baseUrl + 'data.js?t='+new Date().getTime(),
+						dataType: 'jsonp'
+					});
+					console.log( 'init data finish...' );
+				}
+			});
+		},
+		
+		renderULById: function ( id ) {
+			var tmpArr,
+				index = $.inArray( id, DATA_MAPPING ),
+				str = '', i, length;
+		
+			if ( index > -1 ) {
+				tmpArr = this.loadArrayByIndex( index );
+				if ( tmpArr ) {
+					length = tmpArr.length;
+					for ( i=0; i<length; i++ ) {
+						str += '<li><a href="#detail" title="'+index+'_'+tmpArr[i]+'" callback="onListItemTap">' + tmpArr[i] + '</a></li>';
+					}
+				}
+				$('#list ul').html( str );
+			}
+		},
+		
+		loadArrayByIndex: function ( index ) {
+			var length = ls.length, 
+				i,
+				tmpKey,
+				tmpArr = [];
+		
+			for ( i=0; i<length; i++ ) {
+				tmpKey = ls.key(i);
+				if ( tmpKey.indexOf( index ) == 0 ) {
+					tmpArr.push( this.reviseKey( tmpKey ) );				
+				}
+			}
+			return tmpArr;
+		},
+		
+		reviseKey: function ( key ) {
+			return key.substring( key.indexOf('_') + 1 );
+		},
+		
+		onListItemTap: function ( title, from, id, tmpHref, target ) {
+			var val = this.getValueByKey( title );
+			$('#header #leftBtn').attr( 'href', '#list' );
+			$('#header label').text( this.reviseKey( title ) || appName );
+			$('#detail').html( '<p>' + val + '</p>' );
+			this.addToHistory( title );
+		},
+		
+		getValueByKey: function ( key ) {
+			var val;
+			if ( key ) {
+				val = ls.getItem( key );
+				return val;
+			}
+		},
+		
+		setValueByKey: function ( key, val ) {
+			var ls = window.localStorage;
+			ls.setItem( key, val );
+		},
+		
+		addToHistory: function ( key ) {
+			var h = this.getValueByKey( 'h' ), hObj = JSON.parse( h ),
+				tmpIndex = $.inArray( key, hObj );
+			if ( tmpIndex > -1 ) {
+				hObj.splice( tmpIndex, 1 );
+			}
+			hObj.unshift( key );
+			this.setValueByKey( 'h', JSON.stringify( hObj ) );
+		},
+		
+		showHistory: function () {
+			var arr = this.getValueArrayByKey( 'h' ), 
+				i, len = arr.length, str = '';
+			for ( i=0; i<len; i++ ) {
+				str += '<li><a href="#detail" title="'+arr[i]+'">' + this.reviseKey( arr[i] ) + '</a></li>';
+			}
+			$('#history ul').html( str );
+		},
+		
+		getValueArrayByKey: function ( key ) {
+			var arr;
+			arr = JSON.parse( ls.getItem( key ) );
+			return arr;
+		},
+		
+		initSearch: function () {
+			var that = this;
+			$('#searchBtn').on( 'click', function () {
+				var val = $.trim( $('#searchTxt').val() ),
+					arr, str = '';
+				if ( val.length > 0 ) {
+					arr = that.getSearchResults( val );
+					if ( arr ) {
+						length = arr.length;
+						for ( i=0; i<length; i++ ) {
+							str += '<li><a href="#detail" title="'+arr[i]+'" callback="onListItemTap">' + that.reviseKey( arr[i] ) + '</a></li>';
+						}
+					
+					}
+					$('#search ul').html( str );
+				}
+			});
+		},
+		
+		getSearchResults: function ( txt ) {
+			var length = ls.length, 
+				i,
+				tmpKey,
+				tmpArr = [];
+				
+			for ( i=0; i<length; i++ ) {
+				tmpKey = ls.key(i);
+				if ( tmpKey.toLowerCase().indexOf( txt.toLowerCase() ) > -1 ) {
+					tmpArr.push( tmpKey );				
+				}
+			}
+			return tmpArr;
+		}
+	});
+	
+	$( document ).ready( function () {
+		zside.init();
+	});
+})( window, Zepto, zside );
--- js/zside.js
+++ js/zside.js
@@ -1,82 +1,91 @@
  /**
- *	DESC:	A Simple Library Based On Zepto, For Mobile Device
- *	AUTHOR:	1203A
- *	SINCE:	2013-04-06
- *	EMAIL:	gao_st@126.com
+ *	DESC:		A Simple Library Based On Zepto, For Mobile Device
+ *	AUTHOR:		1203A
+ *	SINCE:		2013-04-06
+ *	EMAIL:		gao_st@126.com
+ * 	VERSION:	1.2
  */
-;( function ( window, $ ) {
-	var NO_NEED_LOAD_DATA = [ '#main' ],
-		DATA_MAPPING = [ 'v', 'css', 'css3', 'js', 'jquery', 'html5', 'php', 'sql', 'srsy', 'about' ];
-		
-	var listScroll;
-	
-	$( document ).ready( function () {
-		bindAnchorEvt();
-		listScroll = new iScroll('list', { checkDOMChanges: true, fadeScrollbar: true });
-	});
-	
-	function bindAnchorEvt () {
-		$('#wrapper, #footer').on( 'click', 'a[href]', function ( e ) {
-			var tmpId = $(this).attr('href');
-			$( tmpId ).css({
-				'-webkit-transition': '-webkit-transform 200ms',
-				'-webkit-transform-origin': '0% 0%', 
-				'-webkit-transform': 'translate(0%, 0%) scale(1) translateZ(0px)',
-				'opacity': 1
-			}).siblings().css({
-				'-webkit-transition': '-webkit-transform 200ms',
-				'-webkit-transform-origin': '0% 0%', 
-				'-webkit-transform': 'translate(100%, 0%) scale(1) translateZ(0px)',
-				'opacity': 0
+
+var zside = ( function ( window, $, undefined ) {
+	var zside = {
+		version: '1.1',
+		initialize: function () {
+			this.bindAnchorEvt();
+		},
+		bindAnchorEvt: function () {
+			$('body').on( 'click', 'a[href]', function ( e ) {
+				var tmpHref = $(this).attr('href'),
+					title = $(this).attr('title'),
+					id = $(this).attr('id'),
+					$tmpMenu,
+					from = $(this).attr('from'),
+					callback = $(this).attr('callback');
+				
+				if ( tmpHref.indexOf( '@' ) == 0 ) {
+					if ( tmpHref.indexOf( 'left' ) > -1 ) {
+						$tmpMenu = $('#leftMenu');
+						if ( $tmpMenu.css('opacity') == 0 ) {
+							$tmpMenu.css( 'opacity', 1 );
+							$( '#main' ).css({
+								'-webkit-transition': '-webkit-transform 200ms',
+								'-webkit-transform-origin': '0% 0%', 
+								'-webkit-transform': 'translate(80%, 0%) scale(1) translateZ(0px)'
+							});
+						} else {
+							$( '#main' ).css({
+								'-webkit-transition': '-webkit-transform 200ms',
+								'-webkit-transform-origin': '0% 0%', 
+								'-webkit-transform': 'translate(0%, 0%) scale(1) translateZ(0px)'
+							});
+							setTimeout( function () {
+								$tmpMenu.css( 'opacity', 0 );
+							}, 200);
+						}
+						
+					} else {
+						$tmpMenu = $('#rightMenu');
+						if ( $tmpMenu.css('opacity') == 0 ) {
+							$tmpMenu.css( 'opacity', 1 );
+							$( '#main' ).css({
+								'-webkit-transition': '-webkit-transform 200ms',
+								'-webkit-transform-origin': '0% 0%', 
+								'-webkit-transform': 'translate(-80%, 0%) scale(1) translateZ(0px)'
+							});
+						} else {
+							$( '#main' ).css({
+								'-webkit-transition': '-webkit-transform 200ms',
+								'-webkit-transform-origin': '0% 0%', 
+								'-webkit-transform': 'translate(0%, 0%) scale(1) translateZ(0px)'
+							});
+							setTimeout( function () {
+								$tmpMenu.css( 'opacity', 0 );
+							}, 200);
+						}
+					}
+				} else {
+					$( tmpHref ).css({
+						'-webkit-transition': '-webkit-transform 200ms',
+						'-webkit-transform-origin': '0% 0%', 
+						'-webkit-transform': 'translate(0%, 0%) scale(1) translateZ(0px)',
+						'opacity': 1
+					}).siblings().css({
+						'-webkit-transition': '-webkit-transform 200ms',
+						'-webkit-transform-origin': '0% 0%', 
+						'-webkit-transform': 'translate(100%, 0%) scale(1) translateZ(0px)',
+						'opacity': 0
+					});
+				}
+				
+				if ( callback ) {
+					window['zside'][ callback ]( title, from, id, tmpHref, this );					
+				}
+				e.preventDefault();
 			});
-			
-			e.preventDefault();
-			
-			if ( tmpId == '#list' ) {
-				tmpId = $(this).attr('id');
-			} 
-			loadDataById( tmpId );
-		});
-	}
-	
-	function loadDataById ( id ) {
-		var index;
-		if ( $.inArray( id, NO_NEED_LOAD_DATA ) > -1 ) {
-			return;
 		}
-		
-		index = $.inArray( id, DATA_MAPPING );
-		if ( index > 0 ) {
-			getData( index, id );
-		}
-		
-	}
-	
-	function getData( index, id ) {
-		var ls = window.localStorage,
-			length = ls.length, 
-			i,
-			tmpKey,
-			tmpArr = [];
-		for ( i=0; i<length; i++ ) {
-			tmpKey = ls.key(i);
-			if ( tmpKey.indexOf( index ) == 0 ) {
-				tmpArr.push( tmpKey.substring( tmpKey.indexOf( '_' ) + 1 ) );
-			}
-		}
-		
-		renderUL( tmpArr, id );
-		
-	}
-	
-	function renderUL ( arr, id ) {
-		var str;
-		if ( arr ) {
-			str = '<li><a href="#detail">' + arr.join('</a></li><li><a href="#detail">') + '</a></li>';
-		}
-		
-		$( '#list' ).find( 'ul' ).html( str );
-		
-		listScroll.scrollTo( 0, 0, 100 );
-	}	
+	};
+	return zside;
 })( window, Zepto );
+
+$( document ).ready( function () {
+	zside.initialize();
+});
